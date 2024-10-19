5b241727278ac2053fb30e00abdfda2113689ccc
devMukulSingh <mukulsingh2276@gmail.com>
2024-10-19 19:20:48 +0530

    build script

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,19 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({
+app.use(
+  cors({
     origin: base_url_client,
-    credentials:true,
-}));
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(`/api/v1/auth`,authRouter)
-app.use(`/api/v1/user`,userRouter)
-
+app.use(`/api/v1/auth`, authRouter);
+app.use(`/api/v1/user`, userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running at PORT ${PORT}`);
-})
+  console.log(`Server is running at PORT ${PORT}`);
+});
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,12 +2,10 @@ import { Router } from "express";
 import { postJobController } from "../controller/userController";
 import isAuthenticated from "../middleware/auth";
 
-
 const userRouter = Router();
 
-userRouter.use(isAuthenticated)
+userRouter.use(isAuthenticated);
 
-userRouter.post(`/post-job`,postJobController)
-
+userRouter.post(`/post-job`, postJobController);
 
 export default userRouter;
